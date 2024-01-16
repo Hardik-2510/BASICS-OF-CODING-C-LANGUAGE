6dd22587041acefa8252d9f7920fb42e9d551991
Hardik-2510 <125751962+Hardik-2510@users.noreply.github.com>
2024-01-16 11:48:45 +0530

    This is Another Method To Sum of Two Numbers.
    In This Method Take Input From User.

---
+++ sum-3.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+     int x;
+     int y;
+     printf("Enter A Number :- ");
+     scanf("%d",&x);
+     printf("Enter A Number :- ");
+     scanf("%d",&y);
+     int sum = x + y ;
+     printf("The Sum Of Two Numbers = %d", sum);
+}
